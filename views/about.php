c29331b69f8b3fed81d3fac9f297bd36dc334eab
muhammed faizal <faizalbtechelectrical@gmail.com>
2025-07-31 20:02:15 +0530

    second commit

--- views/about.php
+++ views/about.php
@@ -1,2 +1,4 @@
+<?php include 'header.php'; ?>
 <h1>About Us</h1>
 <p>Learn more about us here.</p>
+<?php include 'footer.php'; ?>
--- views/home.php
+++ views/home.php
@@ -1,6 +1,41 @@
 <?php include 'header.php'; ?>
 
-<!-- Main Content -->
-<h1>home</h1>
+
+
+<div class="container my-4">
+  <div class="row align-items-top gy-3">
+    
+    <!-- Left column: Heading + Paragraph -->
+    <div class="col-12 col-md-6">
+      <h1 class="mb-3 text-white" style=" font-family: 'Sofia', sans-serif;">Ananthapuri</h1>
+      <p class="mb-2 text-muted">
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        Thiruvananthapuram, the "City of the Lord Anantha,
+        " drapes itself across seven hills where history and divinity intertwine under the shade of coconut palms. 
+        Throughout its avenues, 
+        colonial-era buildings with their deep verandas stand in graceful conversation with the intricate wooden gables of traditional Keralan architecture. 
+        At its core, the majestic Sree Padmanabhaswamy Temple, 
+        a marvel of Dravidian craftsmanship, 
+        radiates a profound spiritual energy that anchors the entire city, nourishing a gentle, 
+        intellectual spirit that flows through its art galleries and bookshops. 
+        This vibrant urban life gives way to breathtaking sunsets along its western edge, 
+        where the golden sands of Kovalam are caressed by the turquoise waves of the Arabian Sea. 
+        As twilight finally descends, the air grows heavy with the scent of jasmine and incense, 
+        wrapping the capital in an enchanting and tranquil embrace.  
+        The southern most district and the capital of Kerala, God's own country, 
+        one who visits Thiruvananthapuram, 
+        visits heaven experiencing the ecstasy of being here</p>
+      <h3 class="mb-3 text-white" style=" font-family: 'Roboto', sans-serif;">Visit! Explore! Enjoy!</h3>                
+    </div>
+
+    <!-- Right column: Image -->
+    <div class="col-12 col-md-6 text-md-end text-center">
+      <img src="views/css/assets/lpimage.jpeg" style="width: 350px; height: 400px; object-fit: cover;" alt="Responsive Image" class="img-fluid rounded shadow-sm">
+    </div>
+
+  </div>
+</div>
+
+
 
 <?php include 'footer.php'; ?>
